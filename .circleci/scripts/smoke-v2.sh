829d906a41a7d405b77b3b0b24cb3edb665516ed
Markus von Rüden <mvr@opennms.com>
2019-10-23 14:30:18 +0200

    POC: Ditch the idea of using TestSuites

--- .circleci/scripts/smoke-v2.sh
+++ .circleci/scripts/smoke-v2.sh
@@ -1,6 +1,27 @@
 #!/bin/sh -e
 
-# TODO MVR probably make generic :D
-TEXT_CLASS="UiTestSuite"
-echo "#### Executing ${TEST_CLASS}"
-mvn -N -DskipTests=false -DskipITs=false -Dit.test=$TEST_CLASS install verify
+find_tests()
+{
+    # Generate surefire test list
+    circleci tests glob **/src/test/java/**/*Test*.java |\
+        sed -e 's#^.*src/test/java/\(.*\)\.java#\1#' | tr "/" "." > surefire_classnames
+    cat surefire_classnames | circleci tests split --split-by=timings --timings-type=classname > /tmp/this_node_tests
+
+    # Generate failsafe list
+    circleci tests glob **/src/test/java/**/*IT*.java |\
+        sed -e 's#^.*src/test/java/\(.*\)\.java#\1#' | tr "/" "." > failsafe_classnames
+    cat failsafe_classnames | circleci tests split --split-by=timings --timings-type=classname > /tmp/this_node_it_tests
+}
+
+# Tests are forked out in separate JVMs, so the Maven runner shouldn't need a big heap
+export MAVEN_OPTS="-Xms256m -Xms256m"
+
+cd ~/project/smoke-test-v2
+echo "#### Executing complete suite of smoke/system tests"
+find_tests
+# Iterate through the tests and stop after the first failure
+for TEST_CLASS in $(cat /tmp/this_node_it_tests)
+do
+  echo "###### Testing: ${TEST_CLASS}"
+  mvn -N -DskipTests=false -DskipITs=false -DfailIfNoTests=false -Dit.test=$TEST_CLASS install verify
+done
--- smoke-test-v2/src/test/java/org/opennms/smoketest/ui/OpenNMSSeleniumIT.java
+++ smoke-test-v2/src/test/java/org/opennms/smoketest/ui/OpenNMSSeleniumIT.java
@@ -28,16 +28,120 @@
 
 package org.opennms.smoketest.ui;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.attribute.PosixFilePermissions;
+
 import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.opennms.smoketest.env.MinimalEnvironment;
+import org.opennms.smoketest.env.OpennmsEnvironment;
+import org.opennms.smoketest.env.PostgresEnvironment;
 import org.opennms.smoketest.selenium.AbstractOpenNMSSeleniumHelper;
+import org.opennms.smoketest.utils.RestClient;
+import org.opennms.smoketest.utils.TestContainerUtils;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.containers.Network;
+
+import com.github.dockerjava.api.command.CreateContainerCmd;
 
 /**
  * Base class for Selenium based testing of the OpenNMS web application.
  */
 public class OpenNMSSeleniumIT extends AbstractOpenNMSSeleniumHelper {
+
+    @ClassRule
+    public static final BrowserWebDriverContainer firefox = (BrowserWebDriverContainer) new BrowserWebDriverContainer()
+            .withCapabilities(getFirefoxOptions())
+            // Don't record. This container can fail and cause test instability - for example if retrieving the
+            // .flv from the container fails, an exception will be thrown causing the test to fail
+            .withRecordingMode(BrowserWebDriverContainer.VncRecordingMode.SKIP, new File("target"))
+            //.withRecordingMode(BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL, new File("target"))
+            .withNetwork(Network.SHARED)
+            // Increase the containers shared memory to 2GB to help prevent Firefox from crashing
+            .withSharedMemorySize(2147483648L)
+            // Non-blocking entropy
+            .withEnv("JAVA_OPTS", "-Djava.security.egd=file:/dev/./urandom")
+            .withEnv("SCREEN_WIDTH", "2048")
+            .withEnv("SCREEN_HEIGHT", "1400")
+            .withCreateContainerCmdModifier(cmd -> {
+                final CreateContainerCmd createCmd = (CreateContainerCmd)cmd;
+                TestContainerUtils.setGlobalMemAndCpuLimits(createCmd);
+
+                // Use this hook to ensure that the downloads directory exists
+                final File downloads = AbstractOpenNMSSeleniumHelper.DOWNLOADS_FOLDER;
+                downloads.mkdirs();
+                try {
+                    // Make the folder world readable/writable
+                    Files.setPosixFilePermissions(downloads.toPath(), PosixFilePermissions.fromString("rwxrwxrwx"));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            })
+            .withFileSystemBind("target/downloads", "/tmp/firefox-downloads");
+
+    public static FirefoxOptions getFirefoxOptions() {
+        final FirefoxOptions options = new FirefoxOptions();
+        options.setProfile(new FirefoxProfile());
+        // Disable browser notifications
+        options.addPreference("dom.webnotifications.enabled", false);
+        // Increase the browser resolution on startup
+        options.addArguments("--width=2048");
+        options.addArguments("--height=1400");
+        // Configure FireFox to download PDFs to disk
+        options.addPreference("browser.download.folderList", 2); // Use for the default download directory the last folder specified for a download
+        options.addPreference("browser.download.dir", "/tmp/firefox-downloads"); // Set the last directory used for saving a file from the "What should (browser) do with this file?" dialog.
+        options.addPreference("browser.helperApps.neverAsk.saveToDisk", "application/pdf"); // List of MIME types to save to disk without asking what to use to open the file
+        options.addPreference("pdfjs.disabled", true);  // Disable the built-in PDF viewer
+        // Debug Selenium <-> Firefox
+        //options.setLogLevel(FirefoxDriverLogLevel.TRACE);
+        return options;
+    }
+
+    protected static MinimalEnvironment getEnvironment() {
+        return new MinimalEnvironment() {
+
+            @Override
+            public OpennmsEnvironment opennms() {
+                return new OpennmsEnvironment() {
+
+                    public String getBaseUrlInternal() {
+                        return "http://opennms:8980"; // TODO MVR
+                    }
+
+                    public String getBaseUrlExternal() {
+                        try {
+                            return new URL(String.format("http://%s:%d/", "192.168.1.16", 8980)).toString();
+                        } catch (MalformedURLException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+
+                    @Override
+                    public RestClient getRestClient() {
+                        try {
+                            return new RestClient(new URL(String.format("http://%s:%d/", "192.168.1.16", 8980)));
+                        } catch (MalformedURLException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                };
+            }
+
+            @Override
+            public PostgresEnvironment postgres() {
+                return new PostgresEnvironment() {
+                };
+            }
+        };
+    }
 
     public static RemoteWebDriver driver;
 
@@ -45,8 +149,8 @@ public class OpenNMSSeleniumIT extends AbstractOpenNMSSeleniumHelper {
 
     @BeforeClass
     public static void setUpClass() {
-        driver = UiTestSuite.firefox.getWebDriver();
-        environment = UiTestSuite.getEnvironment();
+        driver = firefox.getWebDriver();
+        environment = getEnvironment();
     }
 
     @Override
